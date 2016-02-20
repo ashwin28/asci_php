caf0d69f93a37e645f653be21b7775f07df15ca1
Ashwin Dey <deyashwin@gmail.com>
2016-02-19 19:38:06 -0500

    updates to rel and src in header

--- email_parser.php
+++ email_parser.php
@@ -3,9 +3,9 @@
 <head>
   <title>Parsed Email Output</title>
   <link rel="stylesheet" type="text/css" href="asci_style.css">
-  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
-  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 </head>
 
 <script>
